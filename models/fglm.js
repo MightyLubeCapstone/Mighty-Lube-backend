7a6c465323e860bb7a5a1fd9d4e85d28dd5fcd90
parsonzachary16 <parsonzachary16@gmail.com>
2025-02-17 19:37:33 -0600

    Fixed more, me add space, now look nice

--- models/fglm.js
+++ models/fglm.js
@@ -4,131 +4,280 @@ const uuid = require("uuid");
 const FGLMSchema = new mongoose.Schema({
     // Gen Info
     conveyorName: {
+
         type: String,
         required: true,
+
     },
+
     chainSize: {
+
         type: Number,
+        enum: [0, 1, 2, 3, 4],
         required: true,
+
     },
+
+    otherChainSize: {
+
+        type: String,
+        required: function()
+	    {
+
+		    return this.chainSize === 4;
+
+	    }
+
+    },
+
     chainManufacturer: {
+
         type: Number,
+        enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
         required: true,
+        
     },
+
+    otherChainManufacturer: {
+
+        type: String,
+        required: function ()
+        {
+            return this.chainManufacturer === 9;
+        }
+
+    },
+
+    wheelManufacturer: {
+
+        type: Number,
+        enum: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        required: true,
+
+    },
+    
+    otherWheelManufacturer: {
+        type: String,
+
+        required: function ()
+        {
+            return this.wheelManufacturer === 9;
+        }
+
+    },
+
     chainPinType: {
+
         type: Number,
+        enum: [0, 1, 2],
         required: true,
         // add RegEx matching to this :D
+
     },
+
     conveyorLength: {
+
         type: String,
         required: true,
         // add RegEx matching to this :D
+
     },
+    
     conveyorLengthUnit: {
+
         type: Number,
+        enum: [0, 1, 2, 3],
         required: true,
         // add RegEx matching to this :D
+
     },
+
     conveyorSpeed: {
+
         type: String,
         required: true,
         // add RegEx matching to this :D
+
     },
+
     conveyorSpeedUnit: {
+
         type: Number,
+        enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
+
     },
+
     conveyorIndex: {
+
         type: String,
         required: true,
         // add RegEx matching to this :D
+
     },
+
     travelDirection: {
+
         type: Number,
+        enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
+
     },
+
     metalType: {
+
         type: Number,
+        enum: [0, 1, 2, 3],
         required: true,
         // add RegEx matching to this :D
+
     },
+
+    otherMetalType: {
+
+        type: String,
+        required: function ()
+        {
+            return this.metalType === 3;
+        }
+
+    },
+
     conveyorStyle: {
+
         type: Number,
+        enum: [0, 1, 2, 3, 4],
         required: true,
         // add RegEx matching to this :D
+
     },
+
+    otherConveyorStyle: {
+
+        type: String,
+        required: function ()
+        {
+            return this.conveyorStyle === 4;
+        }
+
+    },
+
     trolleyColor: {
+
         type: Number,
+        enum: [0, 1, 2, 3],
         required: true,
         // add RegEx matching to this :D
+
     },
+
+    otherTrolleyColor: {
+        
+        type: String,
+        required: function ()
+        {
+            return this.trolleyColor === 3;
+        }
+
+    },
+
     trolleyType: {
+
         type: Number,
+        enum: [0, 1, 2, 3, 4, 5],
         required: true,
         // add RegEx matching to this :D
+
     },
+
     surroundingTemp: {
+
         type: Number,
         enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
+
     },
+
     conveyorLoaded: {
+
         type: Number,
         enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
+
     },
+
     conveyorSwing: {
+
         type: Number,
         enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
+
     },
+
     plantLayout: {
+
         type: Number,
         enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
+
     },
+
     requiredPics: {
+
         type: Number,
         enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
+
     },
+
     // CPU
     operatingVoltage: {
+
         type: String,
         required: true,
+
     },
+
     // MonSys
     existingMonitor: {
+
         type: Number,
         enum: [0, 1],
         required: true,
+
     },
 
     newMonitor: {
+
         type: Number,
         enum: [0, 1],
         required: true,
+
     },
 
     dcuStatus: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1 || this.newMonitor === 1;
         }
+        
     },
 
     dcuNum: {
+
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.dcuStatus === 1;
         }
 
@@ -138,7 +287,8 @@ const FGLMSchema = new mongoose.Schema({
 
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1;
         }
 
@@ -148,7 +298,8 @@ const FGLMSchema = new mongoose.Schema({
 
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1;
         }
 
@@ -158,7 +309,8 @@ const FGLMSchema = new mongoose.Schema({
 
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1;
         }
 
@@ -168,193 +320,276 @@ const FGLMSchema = new mongoose.Schema({
 
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1;
         }
 
     },
 
     newReservoir: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1 || this.newMonitor === 1;
         }
+
     },
 
     reservoirSize: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.newReservoir === 1;
         }
+
+    },
+
+    otherReservoirSize: {
+
+        type: String,
+        required: function()
+        {
+            return this.reservoirSize === 2;
+        }
+
     },
 
     newReservoirNum: {
+
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.newReservoir === 1;
         }
+
     },
 
     typeMonitor: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1 || this.newMonitor === 1;
         }
+
     },
 
     driveMotorAmp: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.typeMonitor === 1;
         }
+
     },
 
     driveMotorAmpNum: {
+
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.driveMotorAmp === 1;
         }
+
     },
 
     driveTakeUpAir: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.typeMonitor === 1;
         }
+
     },
 
     driveTakeUpAirNum: {
+
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.driveTakeUpAir === 1;
         }
+
     },
 
     takeUpDistance: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.typeMonitor === 1;
         }
+
     },
 
     takeUpDistanceNum: {
+
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.takeUpDistance === 1;
         }
+
     },
 
     driveTemp: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.typeMonitor === 1;
         }
+
     },
 
     driveTempNum: {
+
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.driveTemp === 1;
         }
+
     },
 
     driveVibration: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.typeMonitor === 1;
         }
+
     },
 
     driveVibrationNum: {
+
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.driveVibration === 1;
         }
+
     },
 
     dogPitch: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.typeMonitor === 1;
         }
+
     },
 
     dogPitchNum: {
+
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.dogPitch === 1;
         }
+
     },
 
     paintMarker: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1 || this.newMonitor === 1;
         }
+
     },
 
     paintMarkerNum: {
+
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.paintMarker === 1;
         }
+
     },
 
     chainVision: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.typeMonitor === 1;
         }
+
     },
 
     lubeVision: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.typeMonitor === 1;
         }
+
     },
 
     trolleyVision: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.typeMonitor === 1;
         }
+
     },
 
     trolleyDetect: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.trolleyVision === 1;
         }
+
     },
 
     omniView: {
+
         type: Number,
         enum: [0, 1],
-        required: function () {
+        required: function () 
+        {
             return this.typeMonitor === 1;
         }
+        
     },
 
     dcuUpgradeNum: {
+
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.chainVision === 1 || this.omniView === 1;
         }
+
     },
 
     itNameOne: {
@@ -495,7 +730,8 @@ const FGLMSchema = new mongoose.Schema({
     piuDistance: {
 
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1 || this.newMonitor === 1;
         }
 
@@ -505,7 +741,8 @@ const FGLMSchema = new mongoose.Schema({
     switchDistance: {
 
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1 || this.newMonitor === 1;
         }
 
@@ -515,7 +752,8 @@ const FGLMSchema = new mongoose.Schema({
     ampPickup: {
 
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1 || this.newMonitor === 1;
         }
 
@@ -525,7 +763,8 @@ const FGLMSchema = new mongoose.Schema({
     fromAirTakeUpDistance: {
 
         type: Number,
-        required: function () {
+        required: function () 
+        {
             return this.existingMonitor === 1 || this.newMonitor === 1;
         }
 
@@ -540,37 +779,59 @@ const FGLMSchema = new mongoose.Schema({
     },
     // ConveyorSpecs
     sideLube: {
+
         type: Number,
         enum: [0, 1],
         required: true,
+
     },
+
     topLube: {
+
         type: Number,
         enum: [0, 1],
         required: true,
+
     },
+
     cleanChain: {
+
         type: Number,
         enum: [0, 1],
         required: true,
+
     },
+    
     // Wire
     measureUnit: {
+
         type: Number,
+        enum: [0, 1],
         required: true,
+
     },
+
     conductor4: {
+
         type: String,
         required: true,
+
     },
+
     conductor7: {
+
         type: String,
         required: true,
+
     },
+
     conductor2: {
+
         type: String,
         required: true,
+
     },
+
 });
 
 const FGLM = mongoose.models.FGLM || mongoose.model('FGLM', FGLMSchema);
