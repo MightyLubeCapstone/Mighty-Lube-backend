5a7f75f36c5f4e3d3e76a8234c0f8e62646741c6
parsonzachary16 <parsonzachary16@gmail.com>
2025-02-17 19:15:43 -0600

    Hope this is how they want it

--- models/fglm.js
+++ models/fglm.js
@@ -71,27 +71,32 @@ const FGLMSchema = new mongoose.Schema({
         // add RegEx matching to this :D
     },
     surroundingTemp: {
-        type: Boolean,
+        type: Number,
+        enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
     },
     conveyorLoaded: {
-        type: Boolean,
+        type: Number,
+        enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
     },
     conveyorSwing: {
-        type: Boolean,
+        type: Number,
+        enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
     },
     plantLayout: {
-        type: Boolean,
+        type: Number,
+        enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
     },
     requiredPics: {
-        type: Boolean,
+        type: Number,
+        enum: [0, 1],
         required: true,
         // add RegEx matching to this :D
     },
@@ -102,49 +107,451 @@ const FGLMSchema = new mongoose.Schema({
     },
     // MonSys
     existingMonitor: {
-        type: Boolean,
+        type: Number,
+        enum: [0, 1],
         required: true,
     },
+
     newMonitor: {
-        type: Boolean,
+        type: Number,
+        enum: [0, 1],
         required: true,
     },
-    // MonFeatures
-    motorAmp: {
-        type: Boolean,
-        required: true,
+
+    dcuStatus: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.existingMonitor === 1 || this.newMonitor === 1;
+        }
     },
-    takeUpAir: {
-        type: Boolean,
-        required: true,
+
+    dcuNum: {
+        type: Number,
+        required: function () {
+            return this.dcuStatus === 1;
+        }
+
     },
-    takeUpDist: {
-        type: Boolean,
-        required: true,
+
+    existingWindows: {
+
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.existingMonitor === 1;
+        }
+
     },
-    motorTemp: {
-        type: Boolean,
-        required: true,
+
+    existingHeadUnit: {
+
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.existingMonitor === 1;
+        }
+
     },
-    motorVib: {
-        type: Boolean,
-        required: true,
+
+    existingDCU: {
+
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.existingMonitor === 1;
+        }
+
     },
-    detectFaultyTrolley: {
-        type: Boolean,
-        required: true,
+
+    existingPowerInterface: {
+
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.existingMonitor === 1;
+        }
+
+    },
+
+    newReservoir: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.existingMonitor === 1 || this.newMonitor === 1;
+        }
+    },
+
+    reservoirSize: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.newReservoir === 1;
+        }
+    },
+
+    newReservoirNum: {
+        type: Number,
+        required: function () {
+            return this.newReservoir === 1;
+        }
+    },
+
+    typeMonitor: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.existingMonitor === 1 || this.newMonitor === 1;
+        }
+    },
+
+    driveMotorAmp: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.typeMonitor === 1;
+        }
+    },
+
+    driveMotorAmpNum: {
+        type: Number,
+        required: function () {
+            return this.driveMotorAmp === 1;
+        }
+    },
+
+    driveTakeUpAir: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.typeMonitor === 1;
+        }
+    },
+
+    driveTakeUpAirNum: {
+        type: Number,
+        required: function () {
+            return this.driveTakeUpAir === 1;
+        }
+    },
+
+    takeUpDistance: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.typeMonitor === 1;
+        }
+    },
+
+    takeUpDistanceNum: {
+        type: Number,
+        required: function () {
+            return this.takeUpDistance === 1;
+        }
+    },
+
+    driveTemp: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.typeMonitor === 1;
+        }
+    },
+
+    driveTempNum: {
+        type: Number,
+        required: function () {
+            return this.driveTemp === 1;
+        }
+    },
+
+    driveVibration: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.typeMonitor === 1;
+        }
+    },
+
+    driveVibrationNum: {
+        type: Number,
+        required: function () {
+            return this.driveVibration === 1;
+        }
+    },
+
+    dogPitch: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.typeMonitor === 1;
+        }
+    },
+
+    dogPitchNum: {
+        type: Number,
+        required: function () {
+            return this.dogPitch === 1;
+        }
+    },
+
+    paintMarker: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.existingMonitor === 1 || this.newMonitor === 1;
+        }
+    },
+
+    paintMarkerNum: {
+        type: Number,
+        required: function () {
+            return this.paintMarker === 1;
+        }
+    },
+
+    chainVision: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.typeMonitor === 1;
+        }
+    },
+
+    lubeVision: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.typeMonitor === 1;
+        }
+    },
+
+    trolleyVision: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.typeMonitor === 1;
+        }
+    },
+
+    trolleyDetect: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.trolleyVision === 1;
+        }
+    },
+
+    omniView: {
+        type: Number,
+        enum: [0, 1],
+        required: function () {
+            return this.typeMonitor === 1;
+        }
+    },
+
+    dcuUpgradeNum: {
+        type: Number,
+        required: function () {
+            return this.chainVision === 1 || this.omniView === 1;
+        }
+    },
+
+    itNameOne: {
+
+        type: String,
+        required: false,
+
+    },
+
+    itIPOne: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itGatewayOne: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itSubnetOne: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itDNSOne: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itSMTPOne: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itNameTwo: {
+
+        type: String,
+        required: false,
+
+    },
+
+    itIPTwo: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itGatewayTwo: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itSubnetTwo: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itDNSTwo: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itSMTPTwo: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itNameThree: {
+
+        type: String,
+        required: false,
+
+    },
+
+    itIPThree: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itGatewayThree: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itSubnetThree: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itDNSThree: {
+
+        type: Number,
+        required: false,
+
+    },
+
+    itSMTPThree: {
+
+        type: Number,
+        required: false,
+
+    },
+
+
+    itAdditionalNotes: {
+
+        type: String,
+        required: false,
+
+    },
+
+
+    piuDistance: {
+
+        type: Number,
+        required: function () {
+            return this.existingMonitor === 1 || this.newMonitor === 1;
+        }
+
+    },
+
+
+    switchDistance: {
+
+        type: Number,
+        required: function () {
+            return this.existingMonitor === 1 || this.newMonitor === 1;
+        }
+
+    },
+
+
+    ampPickup: {
+
+        type: Number,
+        required: function () {
+            return this.existingMonitor === 1 || this.newMonitor === 1;
+        }
+
+    },
+
+
+    fromAirTakeUpDistance: {
+
+        type: Number,
+        required: function () {
+            return this.existingMonitor === 1 || this.newMonitor === 1;
+        }
+
+    },
+
+
+    specialControllerOptions: {
+
+        type: Number,
+        enum: [0, 1, 2],
+
     },
     // ConveyorSpecs
     sideLube: {
-        type: Boolean,
+        type: Number,
+        enum: [0, 1],
         required: true,
     },
     topLube: {
-        type: Boolean,
+        type: Number,
+        enum: [0, 1],
         required: true,
     },
     cleanChain: {
-        type: Boolean,
+        type: Number,
+        enum: [0, 1],
         required: true,
     },
     // Wire
