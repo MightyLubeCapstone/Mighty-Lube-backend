da5ff5186900ae218dff63ad6d8754e297646261
Wes Talley <wesatalley@gmail.com>
2025-03-03 17:49:59 -0600

    Emails are now sending! Made init route for forgot-password

--- routes/email.js
+++ routes/email.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const nodemailer = require('nodemailer');
-
+const { authenticate } = require("../routes/sessions")
+const mappings = require("../models/mappings")
+const mongoose = require("mongoose")
 
 const router = express.Router();
 
@@ -13,22 +15,84 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-router.post('/send-email', async (req, res) => {
+function getMappedInfo(order) {
+  const modelName = order.productType ? order.productType : null;
+
+  const modelMapping = mappings[`${modelName}_Mapping`];
+
+  function mapValues(field, selectedValue) {
+    if (!modelMapping || !modelMapping[field]) {
+      return selectedValue;
+    }
+    else {
+      if (modelMapping[field] != null) {
+        return modelMapping[field][selectedValue.toString()];
+      }
+    }
+  }
+
+  let mappedInfo = { ...order.productConfigurationInfo };
+
+  // ✅ Map top-level fields
+  if (modelMapping) {
+    Object.keys(modelMapping).forEach(field => {
+      if (mappedInfo[field] !== undefined) {
+        mappedInfo[field] = mapValues(field, mappedInfo[field]);
+      }
+    });
+
+  }
+  return mappedInfo;
+}
+
+router.post('/send-email', authenticate, async (req, res) => {
+  try {
+    const configuration = req.user.configurations[req.user.configurations.length - 1];
+    const email = req.user["email"];
+    if (!email) {
+      return res.status(400).json({ error: 'Invalid email address' });
+    }
+    let emailContent = "";
+    configuration.cart.forEach((order) => {
+      emailContent += `\n\n\n#################################################\n${order.productType}\n\nRequested number of this item: ${order.numRequested}\n`;
+      order = getMappedInfo(order);
+      delete order._id; // Remove the _id property
+      Object.entries(order).forEach(([key, value]) => {
+        emailContent += `\n${key}:\t${value}\n`;
+      });
+    })
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: configuration.configurationName,
+      text: emailContent,
+    };
+
+    await transporter.sendMail(mailOptions);
+    res.status(201).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error('Email error:', error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+router.post('/forgot', async (req, res) => {
   try {
     const { email } = req.body;
     if (!email) {
       return res.status(400).json({ error: 'Invalid email address' });
     }
-
+    
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: email,
-      subject: 'Does this work?',
-      text: 'If this sent, it worked!',
+      subject: "Mighty Lube Password Reset",
+      text: "Your temporary password is 123456",
     };
 
     await transporter.sendMail(mailOptions);
-    res.json({ message: 'Email sent successfully' });
+    res.status(201).json({ message: 'Email sent successfully' });
   } catch (error) {
     console.error('Email error:', error.message);
     res.status(500).json({ error: error.message });
