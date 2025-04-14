306b1391126f94115f0d4cddd14a9c398c218d84
Wes Talley <wesatalley@gmail.com>
2025-04-14 09:02:43 -0500

    Removed conditional .env stuff for db connect

--- config/config.js
+++ config/config.js
@@ -5,14 +5,8 @@ configDotenv();
 async function dbConnect() {
 	//  connect using the URI
 	if (mongoose.connection.readyState === 0) {
-		if (process.env.PRODUCTION == 1) {
-			await mongoose.connect(process.env.MONGODB_URI);
-			console.log("Connected to MongoDB");
-		}
-		else {
-			await mongoose.connect(process.env.MONGODB_LOCAL_URI);
-			console.log("Connected to MongoDB locally");
-		}
+		await mongoose.connect(process.env.MONGODB_URI);
+		console.log("Connected to MongoDB");
 	}
 }
 
