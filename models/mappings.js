17d2a423cadf7fac8218de31562b9d27729ca0b1
Wes Talley <wesatalley@gmail.com>
2025-04-14 09:01:07 -0500

    One more...

--- models/mappings.js
+++ models/mappings.js
@@ -1,6 +1,6 @@
 module.exports = {
-    FGCO_Mapping: require("./FGCO_Mapping.js"),
-    FGLM_Mapping: require("./FGLM_Mapping.js"),
+    FGCO_Mapping: require("./fgco_Mapping.js"),
+    FGLM_Mapping: require("./fglm_Mapping.js"),
     CC5_CL_Mapping: require("./CC5_CL_Mapping.js"),
     CC5_OP4OE_Mapping: require("./CC5_OP4OE_Mapping.js")
 };
