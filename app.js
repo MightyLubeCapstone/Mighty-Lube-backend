9ee294e39eae270e5c75289c13ff54db87a6b030
Wes Talley <wesatalley@gmail.com>
2025-05-31 14:13:05 -0500

    Going back to 3030, that didn't work...

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use("/api/email", emailRoute);
 
 
 const port = process.env.PORT || 3030;
-app.listen(80, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
 	dbConnect();
 	console.log(`Listening on port ${port}...`);
 });
