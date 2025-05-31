63b18798ea1f4e83899225dc0daf227617c43e55
Wes Talley <wesatalley@gmail.com>
2025-05-31 13:41:28 -0500

    Should work now...

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function dbConnect() {
 	//  connect using the URI
 	if (mongoose.connection.readyState === 0) {
-		await mongoose.connect("mongodb://127.0.0.1:27017/mightylube");
+		await mongoose.connect("mongodb://127.0.0.1:27017/mightylube"); // hardcoded this for local VM access
 		console.log("Connected to MongoDB");
 	}
 }
